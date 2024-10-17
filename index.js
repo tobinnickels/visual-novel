feede95e59e6db89e471e8ba9ac3812cc311c9b8
Tobin Nickels <tobinn@arizona.edu>
2024-10-16 17:16:23 -0700

    fixed text not appearing. Merged background and textBox

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
         const stage = new createjs.Stage("Canvas");
 
         // Image sources
-        const background = new Background(["bruh.jpg", "cursed.jpeg"],stage)
-        const textbox = new TextBox(["hee","hoo"],stage)
+        const background = new Background(["bruh.jpg", "cursed.jpeg"],["hee","hoo"],stage)
 
         // Add event listener for canvas click to change the image
         stage.on("stagemousedown", background.changeImage.bind(background));
-        stage.on("stagemousedown", textbox.changeText.bind(textbox))
 
         // Enable tick to keep the stage updated
         createjs.Ticker.framerate = 60;
